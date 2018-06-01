b668873709f4b30e71aa8587c163d518c9998aeb
jessica stenning <jessicastenning@hotmail.co.uk>
2018-06-01 15:57:04 +0100

    updates sample-test for describe and it statements

--- spec/sample-test.js
+++ spec/sample-test.js
@@ -1,23 +1,27 @@
-assert.toBeTrue(true);
-assert.toBeTrue(false);
+describe ("Sample tests");
+  it('tests the to be true function');
+  assert.toBeTrue(true);
+  assert.toBeTrue(false);
 
-assert.toBeFalse(false);
-assert.toBeFalse(true);
+  it('tests to be false function');
+    assert.toBeFalse(false);
+    assert.toBeFalse(true);
 
-assert.toEq("hi", "hi");
-assert.toEq("hi", "hey");
+  it('tests to equal function')
+    assert.toEq("hi", "hi");
+    assert.toEq("hi", "hey");
 
-assert.toEq(1, 1);
-assert.toEq(1, 2);
+    assert.toEq(1, 1);
+    assert.toEq(1, 2);
 
-assert.toEq([1, 2, 3], [1, 2, 3])
-assert.toEq([1, 2, 3], [1, 3, 2])
+    assert.toEq([1, 2, 3], [1, 2, 3]);
+    assert.toEq([1, 2, 3], [1, 3, 2]);
 
-assert.toEq({name: "jess"}, {name: "jess"})
-assert.toEq({name: "jess"}, {name: "ellie"})
+    assert.toEq({name: "jess"}, {name: "jess"});
+    assert.toEq({name: "jess"}, {name: "ellie"});
 
-assert.toInclude("hi jess", "jess");
-assert.toInclude("hi jess", "ellie");
-
-assert.toInclude([1, 2, 3], 3)
-assert.toInclude([1, 2, 3], 4)
+  it('tests to include function')
+    assert.toInclude("hi jess", "jess");
+    assert.toInclude("hi jess", "ellie");
+    assert.toInclude([1, 2, 3], 3);
+    assert.toInclude([1, 2, 3], 4);
